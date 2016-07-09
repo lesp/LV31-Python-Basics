9300ea276cdc7bd2904031cf938be4f4f6931188
Les Pounder <lespounder@gmail.com>
2016-07-09 18:20:56 +0100

    Updated code and removed some cruft

--- Project-3-Pick-A-Colour.py
+++ Project-3-Pick-A-Colour.py
@@ -1,13 +1,11 @@
 from gpiozero import LED, Button
 from time import sleep
-from random import choice
 red = LED(17)
 amber = LED(27)
 green = LED(22)
 red_button = Button(2)
 amber_button = Button(3)
 green_button = Button(4)
-flashes = 3
 LEDS = [red,amber,green]
 def lightup(colour):
     for led in LEDS:
@@ -16,7 +14,7 @@ def lightup(colour):
         red.on()
     elif colour == "amber":
         amber.on()
-    else:
+    elif colour == "green":
         green.on()
 while True:
     if red_button.is_pressed:
